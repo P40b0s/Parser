dfb77450418cb2ed6d98e58f302baeabc3617822
phobio <aaa@gmail.com>
2022-01-19 11:02:14 +0300

    add gitignore

--- Lexer/Tokenizer/ITokenizer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Lexer.Tokenizer
-{
-    public interface ITokenizer<T>
-    {
-        IEnumerable<Token<T>> Tokenize(string query, List<TokenDefinition<T>> definitions);
-    }
-}
