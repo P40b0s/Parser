754df6d8d81cadbcec6b414bf7a5ef7593223755
phobio <aaa@gmail.com>
2022-01-27 12:11:51 +0300

    ParserResult

--- DocumentParser/Parsers/ParseResult.cs
+++ DocumentParser/Parsers/ParseResult.cs
@@ -2,5 +2,6 @@ namespace DocumentParser.Parsers;
 public struct ParseResult
 {
     public int BodyHeadersCount {get;set;}
-    
+    //кодичество заголовков итемов какого уровня приложений абзцев необраь=ботанных нод итд.
+
 }
