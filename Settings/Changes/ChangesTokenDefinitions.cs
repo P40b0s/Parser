7b2175864bd51d6d98e5da2476a7b76f6e470345
p40b0s <p4@mail.ru>
2022-03-20 10:58:26 +0300

    изменил токены парсера изменений

--- Settings/Changes/ChangesTokenDefinitions.cs
+++ Settings/Changes/ChangesTokenDefinitions.cs
@@ -8,6 +8,7 @@ public class ChangesTokenDefinitions : TokenDefinitionBase<ChangesTokenType>
         addToken(ChangesTokenType.NextIsChange, "(изложить|изложив\\s*его)\\s*в\\s*следующей\\s*редакции\\s*:\\s*\n");
         addToken(ChangesTokenType.Stop, "(?<=\n\\s*)[а-я0-9]+([).])\\s*(в\\s+)?(част|стать|пункт|подпун|разд|правилах)");
         addToken(ChangesTokenType.Stop, "(?<=\n\\s*)[а-я0-9]+([).])\\s*[(]");
+        addToken(ChangesTokenType.Stop, "(?<=\n\\s*)[а-я0-9]+([).])\\s*после\\s+(част|стать|пункт|подпун|разд)");
         addToken(ChangesTokenType.Stop, "(?<=\nв?\\s*)(част[ьи]|стать[июе]|пункт[ае]?|подпункт[ае]?|разд[ела]*|правилах)+\\s+\\S+");
         //Напимер Статья 2
         addToken(ChangesTokenType.Stop, "(?<=\n\\s*)статья\\s+\\d+\\s*\n");
